9bcf850cfd8c4832a4081552c7fcbf35d17e7657
Hug Jannik Elia <jannikelia.hug@students.bfh.ch>
2022-08-28 10:32:40 +0200

    Added dynamic coloring in Dashboard :)

--- src/frontend/flypast/src/app/components/dashboard/dashboard.component.ts
+++ src/frontend/flypast/src/app/components/dashboard/dashboard.component.ts
@@ -10,13 +10,24 @@ import { environment } from 'src/environments/environment';
 })
 export class DashboardComponent implements OnInit {
     object: any;
+    color: any;
 
     constructor(private http: HttpClient) {}
 
     ngOnInit(): void {
         this.http.get(environment.baseUrl + 'dashboard').subscribe((obj) => {
-            console.log(obj);
             this.object = obj;
+            for (let obj of this.object) {
+                let percentage = obj.TotalSlots / obj.ReservedSlots;
+                const element = <HTMLElement>document.getElementById(obj.Attraction.ID);
+                if (percentage > 0.75) {
+                    this.color = "bg-green-600";
+                } else if (percentage > 0.25) {
+                    this.color = "bg-orange-600";
+                } else {
+                    this.color = "bg-red-600";
+                }
+            }
         });
     }
 }
