d376bb8e667f83325dbf1c06a42c6f728129dd1f
Jan Lauber <jan.lauber@protonmail.ch>
2022-08-28 09:54:29 +0200

    extend seed to docker-compose
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- seed/seed.sh
+++ seed/seed.sh
@@ -5,19 +5,32 @@
 
 echo "Seeding the database..."
 
+# check if http://api:8000/ping is up
+while ! curl -s http://api:8000/ping; do
+    echo "Waiting for API to be up..."
+    sleep 1
+done
+
 for file in ./attractions/*.json; do
-    echo "Seeding $file"
-    curl -X POST -H "Content-Type: application/json" -d @$file http://localhost:8000/api/v1/attractions
+    # color orange
+    echo -e "\e[38;5;214m$file\e[0m"
+    curl -X POST -H "Content-Type: application/json" -d @$file http://api:8000/api/v1/attractions
 done
 
 for file in ./tickets/*/*.json; do
-    echo "Seeding $file"
-    curl -X POST -H "Content-Type: application/json" -d @$file http://localhost:8000/api/v1/tickets
+    echo -e "\e[38;5;214m$file\e[0m"
+    curl -X POST -H "Content-Type: application/json" -d @$file http://api:8000/api/v1/tickets
 done
 
-for file in seed/activities/*/*.json; do
-    echo "Seeding $file"
-    curl -X POST -H "Content-Type: application/json" -d @$file http://localhost:8000/api/v1/activities
+for file in ./activities/*.json; do
+    echo -e "\e[38;5;214m$file\e[0m"
+    curl -X POST -H "Content-Type: application/json" -d @$file http://api:8000/api/v1/activities
 done
 
-echo "Database seeded."
+if [ $? -eq 0 ]; then
+    # color green
+    echo -e "\e[32mSeeding successful\e[0m"
+else
+    # color red
+    echo -e "\e[31mSeeding failed\e[0m"
+fi
---
+++ src/backend/controllers/pingController.go
@@ -0,0 +1,7 @@
+package controllers
+
+import "github.com/gofiber/fiber/v2"
+
+func Ping(c *fiber.Ctx) error {
+	return c.SendString("pong")
+}
