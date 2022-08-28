5858b5d175d3171d96f766b7302ef57ce60e3a1a
Tschumi Jonas Michael <jonasmichael.tschumi@students.bfh.ch>
2022-08-28 09:46:51 +0200

    init

--- src/frontend/flypast/src/app/components/dashboard/dashboard.component.ts
+++ src/frontend/flypast/src/app/components/dashboard/dashboard.component.ts
@@ -9,14 +9,14 @@ import { environment } from 'src/environments/environment';
     styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-    object: any;
+    object = [{ free: 20, ges: 15, name: 'Jannik' }, { free: 25, ges: 10, name: 'Elia' }, { free: 15, ges: 5, name: 'Lüscher' }, { free: 20, ges: 15, name: 'Hug' }]
 
     constructor(private http: HttpClient) {}
 
     ngOnInit(): void {
-        this.http.get(environment.baseUrl + 'dashboard').subscribe((obj) => {
-            console.log(obj);
-            this.object = obj;
-        });
+        // this.http.get(environment.baseUrl + 'dashboard').subscribe((obj) => {
+        //     console.log(obj);
+        //     this.object = obj;
+        // });
     }
 }
